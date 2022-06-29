d3d2e5f1d999cea26b1006919fbef3bd3600d3d8
Joshuwa08 <joshuwa008@gmail.com>
2022-06-29 12:18:12 +0530

    sdfr

---
+++ src/test/java/DAY1.java
@@ -0,0 +1,20 @@
+
+
+public class DAY1 {
+	public static void main(String[] args) {
+		System.out.println("start");
+		System.out.println("day 1");
+		System.out.println("day 2");
+		System.out.println("day 3");
+		System.out.println("day 4");
+		System.out.println("day 5");
+		System.out.println("day 6");
+		System.out.println("day 9");
+		System.out.println("day 8");
+		System.out.println("day 7");
+		System.out.println("day 10");
+		System.out.println("end");
+		
+	}
+
+}
